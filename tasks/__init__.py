bb29f4b9faf7f8f5735500e0289504136607784f
Daolang Huang <huangdaolang810@gmail.com>
2025-12-10 12:19:07 +0200

    update repo

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,5 +1,4 @@
 from .location_finding import HiddenLocation
-from .death_process import DeathProcess
 from .psychometric import PsychometricTask
 from .gaussian_process import GPTask
 from .al_benchmarks import BenchmarkTask
